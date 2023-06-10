2284fe87f2b5a6ff1a9686c64c85884fe3072747
rabayarabu <rabu9149@gmail.com>
2023-06-10 13:24:14 +0600

    Fetch data from api

---
+++ src/components/Quotes.js
@@ -0,0 +1,53 @@
+/*eslint-disable*/
+import {useState, useEffect} from 'react';
+
+function Quotes() {
+  const [quoteData, setQuoteData] = useState(['Quote here']);
+  const [error, setError] = useState(0);
+  const [loading, setLoading] = useState(0);
+
+  useEffect(() => {
+    setLoading(1);
+    const getData = async () => {
+      try {
+        const res = await fetch(
+          'https://api.api-ninjas.com/v1/quotes?category=intelligence',
+          {
+            headers: {
+              'X-Api-Key': 'zsSQ8XDoKsjbYBs9x2btng==eYn4H43sDACXckgF',
+            },
+          }
+        );
+
+        if (!res.ok) {
+          setError(1);
+          return;
+        }
+
+        const data = await res.json();
+
+        if (Array.isArray(data) && data.length > 0) {
+          setLoading(0);
+          setQuoteData(data[0]);
+        } else {
+          setError(1);
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+    getData();
+  }, []);
+
+  if (error) return <h3>Something went wrong!</h3>;
+  if (loading) return <h4>Loading....</h4>;
+
+  return (
+    <div className='qoute-cont'>
+      <p>{quoteData && quoteData.quote}</p>
+      <h2>{quoteData.author}</h2>
+    </div>
+  );
+}
+
+export default Quotes;
